19a6f715f746b7694c838b804e44721ebd303d52
Destroyer101234 <153321083+Destroyer101234@users.noreply.github.com>
2024-10-17 16:48:24 +0000

    updates

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
-
+name = input()
+password = input()
+if name == "Liam Monteith" and password == "ShutUpLittleKid":
+    print("Welcome to live chaos!")
+    print("Please create a anomoly.")
+    cmd1 = input()
+    if cmd1 == "kill":
+        print("Process stopped.")
+else:
+    print("Password denied.")
